c4cdf9d2eac364ab80f0847dcfb8154ef722b255
mathew-fleisch <mathew.fleisch@gmail.com>
2020-07-01 07:31:47 -0700

    proof of concept

---
+++ brrrt.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+EXPECTED_VARIABLES="GIT_TOKEN GIT_OWNER GIT_REPOSITORY GIT_ACTION"
+for expect in $EXPECTED_VARIABLES; do
+  if [ -z "${!expect}" ]; then
+    echo "Missing environment variable $expect"
+    exit 1
+  fi
+done
+
+if [ -z "$1" ]; then
+    echo "Must pass list of ips"
+    exit 1
+fi
+ips="$1"
+number_pids_per_container=2
+pid_count=0
+payload=""
+
+# Iterate through text file, line by line
+while IFS= read -r ip; do
+  # skip blank lines
+  if ! [ -z $ip ]; then
+    pid_count=$((pid_count+1))
+    echo "IP: $ip"
+    payload="$payload,$ip"
+    # Send off payload, if the pid_count == $number_pids_per_container
+    if [ $pid_count -eq $number_pids_per_container ]; then
+      # Remove leading comma from payload
+      payload=$(echo $payload | sed -e 's/^,//g')
+      echo "Payload: $payload"
+
+      # Send payload
+      curl -s -H "Accept: application/vnd.github.everest-preview+json" \
+        -H "Authorization: token ${GIT_TOKEN}" \
+        --request POST \
+        --data '{"event_type": "'$GIT_ACTION'", "client_payload": { "ips": "'$payload'" }' \
+        https://api.github.com/repos/${GIT_OWNER}/${GIT_REPOSITORY}/dispatches
+
+      # Reset payload variables
+      payload=""
+      pid_count=0
+    fi
+  fi
+done < "$ips"
---
+++ work.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Must pass comma separated list (no spaces) of ips as first argument"
+    exit 1
+fi
+ips="$1"
+
+# Iterate through csv of ips
+IFS=',' read -ra ADDR <<< "$ips"
+for ip in "${ADDR[@]}"; do
+    echo "IP: $ip"
+    ip_filename=$(echo $ip | sed -e 's/\./-/g')
+
+    # Run some process, save output to log, and send to background
+    traceroute $ip | tee "log-${ip_filename}.txt" &
+done
+
+# Timeout to wait for background processes to finish
+sleep 30
+
+zip logs.zip log-*
+
